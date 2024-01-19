99da9d3cd0f6704d07ad20da56985c6cbf15596d
flexxdarker <ksenzhakmax@gmail.com>
2024-01-19 21:06:41 +0200

    add and delete

---
+++ 02 MVC.Model/Controllers/ProductsController.cs
@@ -0,0 +1,62 @@
+﻿using _02_MVC.Model.Data;
+using _02_MVC.Model.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _02_MVC.Model.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ShopDbContext context;
+
+        public ProductsController(ShopDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var products = context.Products.ToList();
+
+            return View(products);
+        }
+
+        public IActionResult Description()
+        {
+            return View();
+        }
+
+		public IActionResult Create(Product model)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View();
+			}
+
+			context.Products.Add(model);
+			context.SaveChanges();
+
+			return RedirectToAction(nameof(Index));
+		}
+
+		public IActionResult Details(int id)
+		{
+			var product = context.Products.Find(id);
+
+			if (product == null) return NotFound();
+
+			return View(product);
+		}
+
+		public IActionResult Delete(int id)
+		{
+			var product = context.Products.Find(id);
+
+			if (product == null) return NotFound();
+
+			context.Remove(product);
+			context.SaveChanges();
+
+			return RedirectToAction(nameof(Index));
+		}
+	}
+}
