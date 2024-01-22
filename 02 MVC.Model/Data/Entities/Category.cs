9a31134dd39dae1cfadc3dc64bbbaf15e851e799
flexxdarker <ksenzhakmax@gmail.com>
2024-01-22 12:14:38 +0200

    database updated

---
+++ 02 MVC.Model/Data/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace _02_MVC.Model.Data.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Product> Poducts { get; set;}
+    }
+}
--- 02 MVC.Model/Data/Entities/Product.cs
+++ 02 MVC.Model/Data/Entities/Product.cs
@@ -6,9 +6,11 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
-        public string Category { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
         public bool InStock { get; set; }
         public string ImageUrl { get; set; }
         public int Discount { get; set; }
+        public int Rating { get; set; }
     }
 }
